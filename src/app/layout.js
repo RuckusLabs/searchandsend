8e6f308181dc4984b30bc0a208fcaafdddadd2b4
Damian Makki <damian_makki@apple.com>
2023-09-24 19:16:46 -0500

    add favicons

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,21 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Search and Send',
-  description: 'Search and Send program by Staffing Fitness',
+  title: 'Search & Send by Staffing Fitness',
+  description: 'Find your next fitness studio hire with Search & Send by Staffing Fitness',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
       <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;700&display=swap" rel="stylesheet" />
     </head>
       <body>{children}</body>
