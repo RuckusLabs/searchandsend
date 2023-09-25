04ba3aac7710afac20f709c0aa634e53f31d74cd
Damian Makki <damian_makki@apple.com>
2023-09-24 22:33:45 -0500

    move icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,11 +9,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
         <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;700&display=swap" rel="stylesheet" />
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         <meta property="og:title" content="Search & Send by Staffing Fitness" />
         <meta property="og:url" content="https://searchandsend.staffingfitness.com" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="../../public/images/og/share.jpg" />
+        <meta property="og:image" content="/images/og/share.jpg" />
         <meta property="og:description" content="Find your next fitness studio hire with Search & Send by Staffing Fitness" />
 
       </head>
